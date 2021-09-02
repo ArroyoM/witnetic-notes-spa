527b77d289135d1b14a691af74a3c6c0e3960261
Bryan Arroyo <arroyobjm@gmail.com>
2021-09-02 11:53:06 -0600

    Basic Proyect

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import userReducer from "../features/user/userSlice";
+import bookReducer from "../features/book/bookSlice";
+
+const reducer = combineReducers({
+  user: userReducer,
+  book: bookReducer,
+});
+
+const persistConfg = {
+  key: "root",
+  storage: storage,
+  whitelist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfg, reducer);
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
